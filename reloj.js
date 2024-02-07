e86db4ab2a7f90f969a40db69bb6701ea70f17f7
FranciscoRomero01 <89942141+FranciscoRomero01@users.noreply.github.com>
2024-02-06 22:36:50 -0300

    Add files via upload

--- reloj.js
+++ reloj.js
@@ -24,3 +24,26 @@ simplyCountdown('#cuenta', {
 	zeroPad: false,
 	countUp: false
 });
+
+
+var instance = M.Carousel.getInstance(elem);
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instances = M.Carousel.init(elems, options);
+});
+
+// Or with jQuery
+
+  $(document).ready(function(){
+    $('.carousel').carousel();
+  });
+
+
+
+  /* jQuery Method Calls
+    You can still use the old jQuery plugin method calls.
+    But you won't be able to access instance properties.
+
+    $('.carousel').carousel('methodName');
+    $('.carousel').carousel('methodName', paramName);
+  */
